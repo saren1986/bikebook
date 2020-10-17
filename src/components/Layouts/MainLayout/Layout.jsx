8c1251dcc583c206b430c4753f2ee4cf7ef93625
sarmat86 <sarnowskimateusz86@gmail.com>
2020-10-17 15:38:07 +0200

    feature: add topbar user menu, move appbar to own component

--- src/components/Layouts/MainLayout/Layout.jsx
+++ src/components/Layouts/MainLayout/Layout.jsx
@@ -1,96 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Container } from '@material-ui/core';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import AppBar from '@material-ui/core/AppBar';
-import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import Logo from '../Logo/Logo';
 import ConfirmDialog from '../../../UX/Dialogs/AlertDialog';
-import MobileDrawer from '../MobileDrawer/MobileDrawer';
+import TopBar from '../TopBar/TopBar';
 
 const useStyles = makeStyles((theme) => ({
-  TopBar: {
-    boxShadow: 'none',
-  },
   MainContainer: {
     marginTop: '15px',
     [theme.breakpoints.up('md')]: {
       marginTop: '50px',
     },
   },
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  left: {
-    width: '50%',
-    [theme.breakpoints.up('md')]: {
-      width: '25%',
-    },
-  },
-  right: {
-    width: '50%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    [theme.breakpoints.up('md')]: {
-      width: '75%',
-    },
-    '& > *': {
-      marginLeft: '10px',
-    },
-  },
 }));
 const Layout = ({ children }) => {
   const classes = useStyles();
-  const [openDrawer, setOpenDrawer] = useState(false);
-
-  const matchesMd = useMediaQuery((theme) => theme.breakpoints.up('md'));
-  const sectionDesktop = (
-    <>
-      <IconButton aria-label="show 17 new notifications" color="inherit">
-        <Badge badgeContent={17} color="secondary">
-          <NotificationsIcon />
-        </Badge>
-      </IconButton>
-      <IconButton
-        edge="end"
-        aria-label="account of current user"
-        // aria-controls={menuId}
-        aria-haspopup="true"
-        // onClick={handleProfileMenuOpen}
-        color="inherit"
-      >
-        <AccountCircle />
-      </IconButton>
-    </>
-  );
   return (
     <>
-      <AppBar
-        position={matchesMd ? 'static' : 'sticky'}
-        classes={{
-          root: classes.TopBar,
-        }}
-      >
-        <Container className={classes.container}>
-          <div className={classes.left}>
-            <Logo />
-          </div>
-          <div className={classes.right}>
-            <Toolbar>
-              {matchesMd ? sectionDesktop : <MenuIcon onClick={() => setOpenDrawer(true)} />}
-            </Toolbar>
-          </div>
-        </Container>
-      </AppBar>
+      <TopBar />
       <Container className={classes.MainContainer}>
         <Grid
           container
@@ -99,9 +26,6 @@ const Layout = ({ children }) => {
           {children}
         </Grid>
       </Container>
-      {matchesMd
-        ? null
-        : <MobileDrawer open={openDrawer} closeHandler={() => setOpenDrawer(false)} />}
       <ConfirmDialog />
     </>
   );
---
+++ src/components/Layouts/TopBar/TopBar.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Container } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import MobileDrawer from '../MobileDrawer/MobileDrawer';
+import Logo from '../Logo/Logo';
+
+const useStyles = makeStyles((theme) => ({
+  TopBar: {
+    boxShadow: 'none',
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  left: {
+    width: '50%',
+    [theme.breakpoints.up('md')]: {
+      width: '25%',
+    },
+  },
+  right: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    [theme.breakpoints.up('md')]: {
+      width: '75%',
+    },
+    '& > *': {
+      marginLeft: '10px',
+    },
+  },
+}));
+const TopBar = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const matchesMd = useMediaQuery((theme) => theme.breakpoints.up('md'));
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const isMenuOpen = Boolean(anchorEl);
+  const handleUserMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleUserMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const userMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id="user-desktop-menu"
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleUserMenuClose}
+    >
+      <MenuItem onClick={handleUserMenuClose}><Link to="/settings">Settings</Link></MenuItem>
+      <MenuItem onClick={handleUserMenuClose}><Link to="/logout">Logout</Link></MenuItem>
+    </Menu>
+  );
+  const sectionDesktop = (
+    <>
+      <IconButton aria-label="show 17 new notifications" color="inherit">
+        <Badge badgeContent={17} color="secondary">
+          <NotificationsIcon />
+        </Badge>
+      </IconButton>
+      <IconButton
+        edge="end"
+        aria-label="account of current user"
+        aria-controls="user-desktop-menu"
+        aria-haspopup="true"
+        onClick={handleUserMenuOpen}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+    </>
+  );
+  return (
+    <>
+      <AppBar
+        position={matchesMd ? 'static' : 'sticky'}
+        classes={{
+          root: classes.TopBar,
+        }}
+      >
+        <Container className={classes.container}>
+          <div className={classes.left}>
+            <Logo />
+          </div>
+          <div className={classes.right}>
+            <Toolbar>
+              {matchesMd ? sectionDesktop : <MenuIcon onClick={() => setOpenDrawer(true)} />}
+            </Toolbar>
+          </div>
+        </Container>
+      </AppBar>
+      {userMenu}
+      {matchesMd
+        ? null
+        : <MobileDrawer open={openDrawer} closeHandler={() => setOpenDrawer(false)} />}
+    </>
+  );
+};
+
+export default TopBar;
